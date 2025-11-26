bc5186dada790629485994a297d2305c4f58aced
josenildogloboplast-cpu <josenildogloboplast@gmail.com>
2025-11-25 21:43:23 -0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import TaskManager from "./TaskManager";
+import App from "./App.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <TaskManager />
+    <App />
   </React.StrictMode>
 );
